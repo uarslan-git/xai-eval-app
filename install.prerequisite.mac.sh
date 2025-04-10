04b1a77610d177f58d4a4ec0922586535f449492
Vignesh <vikiworks.io@gmail.com>
2025-04-10 22:34:06 +0200

    auto

---
+++ install.prerequisite.mac.sh
@@ -0,0 +1,6 @@
+pip3 install tabulate
+brew install node
+npm init -y
+npm install body-parser express sqlite3
+
+
---
+++ install.prerequisite.ubuntu.sh
@@ -0,0 +1,7 @@
+pip3 install tabulate
+sudo apt update
+sudo apt install -y nodejs
+sudo apt install npm
+npm install body-parser express sqlite3
+
+
